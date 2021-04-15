a564c763f9a654f961e7677440a6b4fc75e94507
isma2020alves <73471932+isma2020alves@users.noreply.github.com>
2021-04-15 12:22:47 +0100

    Implemented Repository Pattern.

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _repo;
+        public ProductRepository(StoreContext repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _repo.Products.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _repo.Products.ToListAsync();
+        }
+    }
+}
