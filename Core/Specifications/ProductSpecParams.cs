efc3cf94f54d9c5f13dfc4968074d7ebb1d509e8
isma2020alves <73471932+isma2020alves@users.noreply.github.com>
2021-05-12 17:36:12 +0100

    Added Search functionality

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -4,7 +4,7 @@ namespace Core.Specifications
     {
         private const int MaxPageSize = 50;
         public int PageIndex {get; set; } = 1;
-        private int _pageSize;
+        private int _pageSize = 6;
         public int PageSize
         {
             get => _pageSize;
@@ -14,6 +14,12 @@ namespace Core.Specifications
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
         public string sort { get; set; }
+        private string _search;
+        public string Search 
+        { 
+            get => _search;
+            set => _search = value.ToLower();
+        }
          
     }
 }
